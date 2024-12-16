d051c219fd7ae1c591d100579ba1cc76f4a5c8f7
rafdhiyaulhaq <m.rafidhiyaulhaq@gmail.com>
2024-12-17 05:22:20 +0700

    fe

---
+++ frontend/src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3QOOU9xhY_x-paNI8HVL9Vc8_3Hs6Bs0",
+  authDomain: "artcommission-uas.firebaseapp.com",
+  projectId: "artcommission-uas",
+  storageBucket: "artcommission-uas.firebasestorage.app",
+  messagingSenderId: "881404158660",
+  appId: "1:881404158660:web:dada6352796523be5cdf91",
+  measurementId: "G-B2HQVFB1KD"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
