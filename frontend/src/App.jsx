c9c8e86756c548023448b4590ba182ab28c53582
rafdhiyaulhaq <m.rafidhiyaulhaq@gmail.com>
2024-12-17 05:34:50 +0700

    hapus index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { Box, Text } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
-      <Box p={5}>
-        <Text>Test Render</Text>
+      <Box p={5} bg="gray.100" minH="100vh">
+        <Text fontSize="2xl" color="blue.500">
+          Test Render
+        </Text>
       </Box>
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
