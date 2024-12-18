1e38c8cad1c52d42c7e2577bd965e46d180f94ce
rafdhiyaulhaq <m.rafidhiyaulhaq@gmail.com>
2024-12-18 07:25:48 +0700

    error prevention

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ChakraProvider, Spinner, Center } from '@chakra-ui/react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './config/firebase';
 
+// Pages
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Commissions from './pages/Commissions';
@@ -12,32 +13,40 @@ function App() {
   const [user, loading] = useAuthState(auth);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <Center h="100vh">
+        <Spinner size="xl" color="blue.500" />
+      </Center>
+    );
   }
 
   return (
     <ChakraProvider>
-      <Router>
+      <Router basename="/ArtCommission-UAS">
         <Routes>
           <Route 
             path="/" 
-            element={user ? <Navigate to="/dashboard" /> : <Login />} 
+            element={user ? <Navigate to="/dashboard" /> : <Login />}
           />
           <Route 
             path="/dashboard" 
-            element={user ? <Dashboard /> : <Navigate to="/" />} 
+            element={user ? <Dashboard /> : <Navigate to="/" />}
           />
           <Route 
             path="/commissions" 
-            element={user ? <Commissions /> : <Navigate to="/" />} 
+            element={user ? <Commissions /> : <Navigate to="/" />}
           />
           <Route 
             path="/commissions/:commissionId" 
-            element={user ? <Commissions /> : <Navigate to="/" />} 
+            element={user ? <Commissions /> : <Navigate to="/" />}
           />
           <Route 
             path="/portfolio" 
-            element={user ? <Portfolio /> : <Navigate to="/" />} 
+            element={user ? <Portfolio /> : <Navigate to="/" />}
+          />
+          <Route 
+            path="*" 
+            element={<Navigate to="/" />}
           />
         </Routes>
       </Router>
